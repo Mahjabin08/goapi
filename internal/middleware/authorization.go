66ca49db34c7c4c6ce1235ed2a70d5a32dab578b
Mahjabin Binte Mannan <mahjabin@Mahjabins-MacBook-Air.local>
2025-07-23 18:47:57 +0600

    adding authorization and error handlers

--- internal/middleware/authorization.go
+++ internal/middleware/authorization.go
@@ -11,5 +11,29 @@ import (
 var UnAuthorizedError = errors.New("Invalid username or token")
 //it needs to take in and return a http handler interface
 func Authorization(next http.Handler) http.Handler {
-	
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		var username string = r.URL.Query().Get("username")
+		token := r.Header.Get("Authorization")
+		var err error
+
+		if username=-"" || token ==""{
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w,UnAuthorizedError)
+			return
+		}
+		var database *tools.DatabaseInterface
+		database, err = tools.NewDatabase()
+		if err != nil{
+			api.InternalErrorHandler(w)
+			return
+		}
+		var loginDetails *tools.loginDetails
+		loginDetails=(*database).GetUserLoginDetails(username)
+		if (loginDetails== nil || (token != (*loginDetails).AuthToken)){
+			log.Error(UnAuthorizedError)
+			api.RequestErrorHandler(w, UnAuthorizedError)
+			return
+		}
+		next.ServeHTTP(w,r)
+	})
 }
